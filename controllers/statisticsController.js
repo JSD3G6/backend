e9f93a41ac59cf845373965a7fe6c663da744277
Pavit Pimchanagul <ppimchan40@gmail.com>
2022-12-18 19:39:43 +0700

    finish:timebound and correct start,end search

--- controllers/statisticsController.js
+++ controllers/statisticsController.js
@@ -6,7 +6,7 @@ const dateUtils = require("../utils/date");
 exports.getStatistics = async (req, res, next) => {
   try {
     const user = req.user.toObject();
-    const { activityType, duration = "week", range } = req.query; // activityType = undefined === allType
+    const { activityType, duration = "week", date } = req.query; // activityType = undefined === allType
 
     let filterCondition = { userId: user._id };
     // filter condition
@@ -25,19 +25,20 @@ exports.getStatistics = async (req, res, next) => {
     }
 
     let start, end;
-    let searchDay = new Date(); // UTC
-    searchDay = searchDay.toISOString().split("T")[0]; // YYYY-MM-DD
+    let searchDayObj = date ? new Date(date) : new Date(); // UTC:000 : UTC with SpecificTime
+    // searchDay = searchDay.toISOString().split("T")[0]; // YYYY-MM-DD
     if (duration === "week") {
-      [start, end] = dateUtils.findWeekInterval(searchDay);
+      [start, end] = dateUtils.findWeekInterval(searchDayObj);
     } else if (duration === "month") {
-      [start, end] = dateUtils.findMonthInterval(searchDay);
+      [start, end] = dateUtils.findMonthInterval(searchDayObj);
     } else if (duration === "year") {
-      [start, end] = dateUtils.findYearInterval(searchDay);
+      [start, end] = dateUtils.findYearInterval(searchDayObj);
     }
-
+    console.log("START", start);
+    console.log("END", end);
     filterCondition.dateTime = {
-      $gte: new Date(start),
-      $lte: new Date(end),
+      $gte: new Date(`${start}T00:00:00.000+0700`),
+      $lt: new Date(`${end}T00:00:00.000+0700`),
     };
 
     const activityLists = await ActivityModel.find(filterCondition);
--- utils/date.js
+++ utils/date.js
@@ -1,26 +1,26 @@
 // UTILS
-const isLeapYear = (year) => {
-  if (year % 100 === 0) {
-    return year % 400 === 0;
-  }
-  return year % 4 === 0;
-};
-const daysInMonth = (month, year) => {
-  switch (month) {
-    case "01":
-    case "03":
-    case "05":
-    case "07":
-    case "08":
-    case "10":
-    case "12":
-      return "31";
-    case "02":
-      return isLeapYear(year) ? "29" : "28";
-    default:
-      return "30";
-  }
-};
+// const isLeapYear = (year) => {
+//   if (year % 100 === 0) {
+//     return year % 400 === 0;
+//   }
+//   return year % 4 === 0;
+// };
+// const daysInMonth = (month, year) => {
+//   switch (month) {
+//     case "01":
+//     case "03":
+//     case "05":
+//     case "07":
+//     case "08":
+//     case "10":
+//     case "12":
+//       return "31";
+//     case "02":
+//       return isLeapYear(year) ? "29" : "28";
+//     default:
+//       return "30";
+//   }
+// };
 
 const convertDateObjToDateString = (dateObj) => {
   let [m, d, y] = dateObj.toLocaleDateString().split("/");
@@ -36,7 +36,7 @@ const findYearInterval = (dateObj) => {
   let year = arrDate[0];
 
   let startYear = `${year}-01-01`;
-  let endYear = `${year}-12-31`;
+  let endYear = `${+year + 1}-01-01`;
   return [startYear, endYear];
 };
 const findMonthInterval = (dateObj) => {
@@ -45,9 +45,16 @@ const findMonthInterval = (dateObj) => {
   let month = arrDate[1];
   let year = arrDate[0];
 
-  let lastDayOfMonth = daysInMonth(month, year);
+  let canNextMonth = month != 12;
+  let newNextMonth;
+  if (canNextMonth) {
+    newNextMonth = "0" + (Number(month) + 1);
+  }
+  let summarizeEndYear = canNextMonth ? year : +year + 1;
+  let summarizeEndMonth = canNextMonth ? newNextMonth : "01";
+  // let lastDayOfMonth = daysInMonth(month, year);
   let startMonth = `${year}-${month}-01`;
-  let endMonth = `${year}-${month}-${lastDayOfMonth}`;
+  let endMonth = `${summarizeEndYear}-${summarizeEndMonth}-01`;
   return [startMonth, endMonth];
 };
 
@@ -59,7 +66,7 @@ const findWeekInterval = (dateObj) => {
 
   // BASE : UNIX
   const mySundayTime = dateObj.getTime() - MS_IN_DAY * dayInWeek;
-  const mySaturdayTime = dateObj.getTime() + MS_IN_DAY * (6 - dayInWeek);
+  const mySaturdayTime = dateObj.getTime() + MS_IN_DAY * (7 - dayInWeek);
   const sunday = new Date(mySundayTime); // gte
   const saturday = new Date(mySaturdayTime); // lte
 
