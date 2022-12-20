b819a6ef8c80b57e7dd86f6930ad5ce6049f749d
Pavit Pimchanagul <ppimchan40@gmail.com>
2022-12-20 15:30:03 +0700

    fixed:deploy

--- mongodb/connection.js
+++ mongodb/connection.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const connect = async () => {
   try {
-    mongoose.set("useNewUrlParser", true);
-    await mongoose.connect(process.env.DB_ENDPOINT); // take time, maybe success or failed
+    // mongoose.set("useNewUrlParser", true);
+    await mongoose.connect(process.env.DB_ENDPOINT, { useNewUrlParser: true }); // take time, maybe success or failed
     console.log("DB Connected");
   } catch (error) {
     console.log(error);
